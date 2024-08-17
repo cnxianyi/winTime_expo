21c7a6ed4eb92381d460b18a7e7a97d941156111
xianyi <thefangyi@gmail.com>
2024-08-17 21:43:54 +0800

    add: react-native-echarts & expo-navigation-bar & react-native-toast-message

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,44 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message'; // 导入 Toast 组件
+const toastConfig = {
+  //@ts-ignore
+  success: ({ text1, text2, ...rest }) => (
+    <BaseToast
+      {...rest}
+      style={{ borderLeftColor: '#37a2da' , height: 100 }}
+      contentContainerStyle={{ paddingHorizontal: 20 }}
+      text1Style={{
+        fontSize: 24, // 设置 text1 的字体大小
+        fontWeight: '400',
+      }}
+      text2Style={{
+        fontSize: 20, // 设置 text2 的字体大小
+        color: 'gray'
+      }}
+      text1={text1}
+      text2={text2}
+    />
+  ),
+  //@ts-ignore
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      text1Style={{
+        fontSize: 20 // 设置 ErrorToast 的字体大小
+      }}
+      text2Style={{
+        fontSize: 16
+      }}
+    />
+  ),
+};
+
+
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View , Text } from 'react-native';
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -32,6 +69,8 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      {/* @ts-ignore */}
+      <Toast config={toastConfig} />
     </ThemeProvider>
   );
 }
